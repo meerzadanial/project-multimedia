b147d7d2e0e9188cf08b9f35251183463e014bb7
Meerza Danial <meerzadanial@gmail.com>
2025-01-04 16:13:01 +0800

    adding color on navbar when change page

--- ProjectMultimedia/src/App.jsx
+++ ProjectMultimedia/src/App.jsx
@@ -4,21 +4,23 @@ import Home from './pages/Home';
 import Products from './pages/Products';
 import About from './pages/about'; // Correct import
 import Contact from './pages/Contact'; // Ensure this exists
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom';
+import RootLayout from './layout/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />} >
+        <Route index element={<Home />} />
+        <Route path='products' element={<Products />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <div className='container'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-      </div>
-    </div>
+   <RouterProvider router={router}/>
   );
 };
 
--- ProjectMultimedia/src/components/Navbar.jsx
+++ ProjectMultimedia/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import logo from '../assets/datanian.png'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div class ="navbar">
+    <div className= "navbar">
       <img src={logo}></img>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Products</li>
-        <li>Contact</li>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/products'><li>Products</li></NavLink>
+        <NavLink to='/about'><li>About</li></NavLink>
+        <NavLink to='/contact'><li>Contact</li></NavLink>
       </ul>
       <button>Get Started</button>
     </div>
